f768be91fe160a82b377c05e6f4e66a6e0b04288
Matrix <timberlake2025@gmail.com>
2022-08-20 11:33:39 -0700

    config changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-//const { token } = require('./config.json');
-const token = process.env.token
+const { token } = require('./config.json');
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
